3d4d1f53fb427f61a69a89f422d833196adb27d9
AdamPanag <adampanag06@gmail.com>
2021-01-18 04:03:08 +0200

    Add draw & highlight winning combination

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Square = ({ value, onClick }) => {
+const Square = ({ value, isWinningSquare, onClick }) => {
   return (
-    <button type="button" className="square" onClick={onClick}>
+    <button
+      type="button"
+      className="square"
+      onClick={onClick}
+      style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}
+    >
       {value}
     </button>
   );
---
+++ src/components/StatusMessage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const StatusMessage = ({ winner, current }) => {
+  //   const message = winner
+  //     ? `Winner is ${winner}`
+  //     : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+
+  const noMovesLeft = current.board.every(el => {
+    return el !== null;
+  });
+
+  return (
+    <h2>
+      {winner && `Winner is ${winner}`}
+      {!winner &&
+        !noMovesLeft &&
+        `Next player is ${current.isXNext ? 'X' : 'O'}`}
+      {!winner && noMovesLeft && 'Draw'}
+    </h2>
+  );
+};
+
+export default StatusMessage;
